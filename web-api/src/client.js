b5d6c4a4c3ce6f43042b426dd4b21d796c37858a
Chinedu Abalogu <chineduabalogu@gmail.com>
2023-10-18 15:13:21 +0100

    wrapup

--- web-api/src/client.js
+++ web-api/src/client.js
@@ -1,5 +1,6 @@
 import { get } from "http";
 import { Transform, Writable } from "stream";
+import { createWriteStream } from "fs";
 
 const url = "http://localhost:3091";
 
@@ -8,11 +9,31 @@ const getHttpStream = () =>
 
 const stream = await getHttpStream();
 
-stream.pipe(
-  Transform({
-    transform(chunk, enc, cb) {
-      console.log("chunk ", JSON.parse(chunk));
-      cb(null, chunk);
-    },
-  })
-);
+stream
+  .pipe(
+    Transform({
+      objectMode: true,
+      transform(chunk, enc, cb) {
+        const item = JSON.parse(chunk);
+
+        const myNum = /\d+/.exec(item.name)[0];
+        const isEven = myNum % 2 === 0;
+        item.name = item.name.concat(isEven ? " is even" : " is odd");
+
+        cb(null, JSON.stringify(item));
+      },
+    })
+  )
+  .filter((chunk) => chunk.includes("even"))
+  .map((chunk) => chunk.toUpperCase() + "\n")
+  .pipe(createWriteStream("response.log", { flag: "a" }));
+// .pipe(
+//   Writable({
+//     objectMode: true,
+//     write(chunk, enc, cb) {
+//       console.log("chunk...", chunk);
+//       return cb();
+//     },
+//   })
+// );
+// .pipe(process.stdout);
